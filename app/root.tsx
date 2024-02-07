f95be3e49c8d165620cdd07c7637ca4a618c2e4c
eleach <eleach2@yahoo.com>
2024-02-07 15:50:08 -0600

    works

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,17 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+// import stylesheet from "~/tailwind.css";
+import "~/tailwind.css";
+
+/* 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
+
+ */
+
+
 export default function App() {
   return (
     <html lang="en">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,109 +1,120 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+import { db } from '~/db.server';
+import { json } from '@remix-run/node';
 
-import { PrismaClient } from "@prisma/client";
-const prisma =  new PrismaClient();
-import { db } from "~/db.server";
-import { json } from "@remix-run/node";
+import { LoaderFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
-import { LoaderFunction } from "@remix-run/node";
-import {  useLoaderData } from "@remix-run/react";
-
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { useState } from 'react';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: 'MoviePairs' }, { name: 'description', content: 'MoviePairs' }];
 };
 
-export let loader: LoaderFunction = async () => {
+export let loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  let a = parseInt(url.searchParams.get('a'));
+  console.log('a: ', a);
+  const b = parseInt(url.searchParams.get('b'));
+  console.log('b: ', b);
 
+  // get-all-movies BEGIN - WORKS
 
-  // const movies = await prisma.$queryRaw`SELECT * FROM assoc WHERE actor = 53`
-  // const movies = await prisma.$queryRaw`SELECT * FROM "assoc" where assoc.actor = 27`
-  // const movies = await prisma.$queryRaw`SELECT * FROM "assoc" where "assoc"."actor" = 27`
+  let aName = await prisma.$queryRaw`SELECT name FROM "actors" WHERE "actors"."myId" = ${a}`;
+  aName = aName[0].name;
 
-  // WORKS
-  // const movies = await prisma.$queryRaw`SELECT * FROM "assoc" JOIN "movies" ON "assoc"."movie" = "movies"."myId" WHERE "assoc"."actor" = 27`
+  let bName = await prisma.$queryRaw`SELECT name FROM "actors" WHERE "actors"."myId" = ${b}`;
+  bName = bName[0].name;
 
-  const actors = await prisma.$queryRaw`SELECT * FROM actors`
-  const movies = await prisma.$queryRaw`SELECT * FROM movies`
-  const assoc = await prisma.$queryRaw`SELECT * FROM assoc`
+  const aActorMovies = await prisma.$queryRaw`SELECT movie FROM "assoc" WHERE "assoc"."actor" = ${a}`;
+  // console.log("aActorMovies: ", aActorMovies)
+  const aActorMoviesNumArray = aActorMovies.map((r) => r.movie);
 
-  // use variable example
-  // const result = await prisma.$queryRaw`SELECT * FROM User WHERE email = ${email}`
+  const bActorMovies = await prisma.$queryRaw`SELECT movie FROM "assoc" WHERE "assoc"."actor" = ${b}`;
+  // console.log("bActorMovies: ", bActorMovies)
+  const bActorMoviesNumArray = bActorMovies.map((r) => r.movie);
 
-  return json({ movies, actors, assoc } );
+  let inter = aActorMoviesNumArray.filter((num) => bActorMoviesNumArray.includes(num));
 
-} 
+  // console.log("inter: ", inter)
+
+  const aMovies = await prisma.$queryRaw`SELECT * FROM "movies" WHERE "movies"."myId" =  ANY(${aActorMoviesNumArray})`;
+  const bMovies = await prisma.$queryRaw`SELECT * FROM "movies" WHERE "movies"."myId" =  ANY(${bActorMoviesNumArray})`;
+
+  return json({ aName, bName, aMovies, bMovies, inter });
+};
+
 
 
 export default function Index() {
+  let { aName, bName, aMovies, bMovies, inter } = useLoaderData<typeof loader>();
 
+  let commonMovies = inter.map((num) => aMovies.find((movie) => movie.myId === num));
 
-  console.log("")
+  let commonMoviesIds = commonMovies.map((movie) => movie.myId);
 
-  console.log(">>>>>")
-  console.log(">>>>>")
-  console.log(">>>>>")
+  let aMoviesFiltered = aMovies.filter((movie) => !commonMoviesIds.includes(movie.myId));
+  let bMoviesFiltered = bMovies.filter((movie) => !commonMoviesIds.includes(movie.myId));
 
+  console.log('commonMovies: ', commonMovies);
 
-  // let movies = useLoaderData();
-  let { movies, actors, assoc } = useLoaderData();
+  const [expandNum, setExpandNum] = useState(0);
 
-  // console.log("m: ", movies.length)
-  // console.log("ac: ", actors.length)
-  // console.log("as: ", assoc.length)
+  function expandMe(id) {
 
-  let stewartMovies = assoc.filter((ass) => ass.actor === 27)
-  let stewartMovieNumsOnly = stewartMovies.map((r) => r.movie)
+    setExpandNum(() => id )
 
-  let hepburnMovies = assoc.filter((ass) => ass.actor === 110)
-  let hepburnMoviesNumsOnly = hepburnMovies.map((movie) => movie.movie)
-
-  let intersection = hepburnMoviesNumsOnly.filter((num) => stewartMovieNumsOnly.includes(num)) 
-
-  let results = movies.filter((m) => intersection.includes(m.myId))
-
+  }
 
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
       <h1>Welcome to Remix</h1>
-      <pre>
-      {
 
-        // JSON.stringify(movies, null, 2)
-        // JSON.stringify(intersection, null, 2)
-        JSON.stringify(results, null, 2)
-}
-</pre>
+      <h2>Common Movies</h2>
+
+      {commonMovies.map((movie) => (
+        <div key={movie.myId}>{movie.name}</div>
+      ))}
+
+      <div className="flex flex-row m-4 ">
+        <div>
+          <h2>{aName}</h2>
+          <ul className=" flex flex-col gap-y-4  ">
+            {aMoviesFiltered.map((movie) => (
+              <div className="rounded-lg  p-2  bg-slate-400 w-18  h-fit">
+
+                <button onClick={() => expandMe(movie.myId)}>
+                <li key={movie.myId}>{movie.name}</li>
+                </button>
+
+                {parseInt(expandNum) == parseInt(movie.myId) && (
+
+                  <div>
+                    Hit<br />
+                    <p>{movie.myId}</p>
+                  </div>
+                )}
 
 
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+
+
+
+              </div>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h2> {bName}</h2>
+          <ul>
+            {bMoviesFiltered.map((movie) => (
+              <li key={movie.myId}>{movie.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
